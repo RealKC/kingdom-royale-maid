e5fc64b291bd05241a160aeb15b02475b620da0d
Mițca Dumitru <dumitru0mitca@gmail.com>
2020-09-28 16:37:17 +0300

    Make GameState more accurate to the book, should make some things easier

--- src/game/game.rs
+++ src/game/game.rs
@@ -6,14 +6,27 @@ type Host = UserId;
 pub struct Game {
     guild: GuildId,
     state: GameState,
+    host: Host,
     players: Vec<Player>,
     day: u8,
 }
 
-enum GameState {
-    NotStarted,
-    Pregame,
-    FirstMeeting,
-    SecretMeetings,
-    SecondMeeting,
+impl Game {
+    pub fn state(&self) -> GameState{
+        self.state
+    }
+}
+
+#[derive(Copy, Clone)]
+pub enum GameState {
+    NotStarted, // Recruiting phase and stuff
+    Pregame, // for giving players an introduction to the game _in character_
+    // Blocks taken from the timetable in the book
+    ABlock, // break, standby in own room
+    BBlock, // Gathering in the big room, "First meeting"
+    CBlock, // Secret meeting partner selection & meeting with them, King, Sorcerer, Knight can act, someone might die during this block
+    DBlock, // Gathering in the big room, "Second meeting"
+    EBlock, // Dinner, no food => death, Revolutionary can act
+    FBlock, // Sleep & Break, is this useful?
+    GameEnded
 }
