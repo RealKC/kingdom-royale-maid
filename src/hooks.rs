a98247fd0a9f2190e8c21cbed8f3023b817a72eb
RealKC <dumitru0mitca@gmail.com>
2020-10-04 21:56:19 +0300

    Move hooks outside of main.rs

---
+++ src/hooks.rs
@@ -0,0 +1,63 @@
+use crate::commands::stats::CommandCounter;
+use serenity::prelude::*;
+use serenity::{
+    framework::standard::{macros::hook, CommandResult, DispatchError},
+    model::channel::Message,
+};
+use tracing::info;
+
+#[hook]
+pub async fn before(ctx: &Context, msg: &Message, command_name: &str) -> bool {
+    info!(
+        "Got command '{}' by user '{}'",
+        command_name, msg.author.name
+    );
+
+    // Increment the number of times this command has been run once. If
+    // the command's name does not exist in the counter, add a default
+    // value of 0.
+    let mut data = ctx.data.write().await;
+    let counter = data
+        .get_mut::<CommandCounter>()
+        .expect("Expected CommandCounter in TypeMap.");
+    let entry = counter.entry(command_name.to_string()).or_insert(0);
+    *entry += 1;
+
+    true // if `before` returns false, command processing doesn't happen.
+}
+
+#[hook]
+pub async fn after(
+    _ctx: &Context,
+    _msg: &Message,
+    command_name: &str,
+    command_result: CommandResult,
+) {
+    match command_result {
+        Ok(()) => info!("Processed command '{}'", command_name),
+        Err(why) => info!("Command '{}' returned error {:?}", command_name, why),
+    }
+}
+
+#[hook]
+pub async fn unknown_command(_ctx: &Context, _msg: &Message, unknown_command_name: &str) {
+    info!("Could not find command named '{}'", unknown_command_name);
+}
+
+#[hook]
+pub async fn normal_message(_ctx: &Context, msg: &Message) {
+    info!("Message is not a command '{}'", msg.content);
+}
+
+#[hook]
+pub async fn dispatch_error(ctx: &Context, msg: &Message, error: DispatchError) {
+    if let DispatchError::Ratelimited(duration) = error {
+        let _ = msg
+            .channel_id
+            .say(
+                &ctx.http,
+                &format!("Try this again in {} seconds.", duration.as_secs()),
+            )
+            .await;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,9 @@ use tokio::sync::Mutex;
 use tracing::{error, info, instrument};
 
 mod commands;
+mod hooks;
 use commands::{game::*, meta::*, random::*, stats::*};
+use hooks::*;
 
 mod game;
 
@@ -92,56 +94,6 @@ async fn my_help(
 ) -> CommandResult {
     let _ = help_commands::with_embeds(context, msg, args, help_options, groups, owners).await;
     Ok(())
-}
-#[hook]
-async fn before(ctx: &Context, msg: &Message, command_name: &str) -> bool {
-    info!(
-        "Got command '{}' by user '{}'",
-        command_name, msg.author.name
-    );
-
-    // Increment the number of times this command has been run once. If
-    // the command's name does not exist in the counter, add a default
-    // value of 0.
-    let mut data = ctx.data.write().await;
-    let counter = data
-        .get_mut::<CommandCounter>()
-        .expect("Expected CommandCounter in TypeMap.");
-    let entry = counter.entry(command_name.to_string()).or_insert(0);
-    *entry += 1;
-
-    true // if `before` returns false, command processing doesn't happen.
-}
-
-#[hook]
-async fn after(_ctx: &Context, _msg: &Message, command_name: &str, command_result: CommandResult) {
-    match command_result {
-        Ok(()) => info!("Processed command '{}'", command_name),
-        Err(why) => info!("Command '{}' returned error {:?}", command_name, why),
-    }
-}
-
-#[hook]
-async fn unknown_command(_ctx: &Context, _msg: &Message, unknown_command_name: &str) {
-    info!("Could not find command named '{}'", unknown_command_name);
-}
-
-#[hook]
-async fn normal_message(_ctx: &Context, msg: &Message) {
-    info!("Message is not a command '{}'", msg.content);
-}
-
-#[hook]
-async fn dispatch_error(ctx: &Context, msg: &Message, error: DispatchError) {
-    if let DispatchError::Ratelimited(duration) = error {
-        let _ = msg
-            .channel_id
-            .say(
-                &ctx.http,
-                &format!("Try this again in {} seconds.", duration.as_secs()),
-            )
-            .await;
-    }
 }
 
 #[tokio::main]
