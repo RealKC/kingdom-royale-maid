2b2643e91ceba96d79c8251b480eeb148d9d7102
RealKC <dumitru0mitca@gmail.com>
2020-10-11 15:34:41 +0300

    Use CreateEmbed::fields for somewhat cleaner code

--- src/commands/game/game_info.rs
+++ src/commands/game/game_info.rs
@@ -8,9 +8,57 @@ use serenity::model::misc::Mentionable;
 pub async fn game_info(ctx: &Context, msg: &Message) -> CommandResult {
     let game = ctx.data.read().await;
     let game = game.get::<GameContainer>();
+
     match game {
         Some(game) => {
             let game = game.write().await;
+
+            let fields = vec![
+                ("Host", game.host().mention(), false),
+                (
+                    "Players",
+                    (|| {
+                        if game.state() == GameState::NotStarted {
+                            let mut players = String::new();
+                            for user in game.joined_users().iter() {
+                                players.push_str(&user.mention());
+                            }
+                            if !players.is_empty() {
+                                players
+                            } else {
+                                "None have joined yet :(".to_string()
+                            }
+                        } else {
+                            let mut players = String::new();
+
+                            let mut all_alive_have_won = false;
+
+                            for player in game.players().iter() {
+                                all_alive_have_won |= player.1.win_condition_achieved(&*game);
+                            }
+
+                            for player in game.players().iter() {
+                                if player.1.is_alive() {
+                                    let mention = player.0.mention();
+                                    players.push_str(&mention);
+                                    if all_alive_have_won {
+                                        players.push_str("(Victory!)");
+                                    }
+                                    players.push('\n');
+                                } else {
+                                    let mention = format! {"~~{}~~\n", player.0.mention()};
+                                    players.push_str(&mention);
+                                }
+                            }
+                            players
+                        }
+                    })(),
+                    true,
+                ),
+                ("Meeting room", game.meeting_room().mention(), true),
+                ("Player role", game.player_role().mention(), true),
+            ];
+
             msg.channel_id
                 .send_message(ctx, |m| {
                     m.embed(|e| {
@@ -35,48 +83,7 @@ pub async fn game_info(ctx: &Context, msg: &Message) -> CommandResult {
                             }
                         })
                         .title("Kingdom Royale")
-                        .field("Host", game.host().mention(), false)
-                        .field(
-                            "Players",
-                            (|| {
-                                if game.state() == GameState::NotStarted {
-                                    let mut players = String::new();
-                                    for user in game.joined_users().iter() {
-                                        players.push_str(&user.mention());
-                                    }
-                                    if !players.is_empty() {
-                                        players
-                                    } else {
-                                        "None have joined yet :(".to_string()
-                                    }
-                                } else {
-                                    let mut players = String::new();
-
-                                    let mut all_alive_have_won = false;
-
-                                    for player in game.players().iter() {
-                                        all_alive_have_won |=
-                                            player.1.win_condition_achieved(&*game);
-                                    }
-
-                                    for player in game.players().iter() {
-                                        if player.1.is_alive() {
-                                            let mention = player.0.mention();
-                                            players.push_str(&mention);
-                                            if all_alive_have_won {
-                                                players.push_str("(Victory!)");
-                                            }
-                                            players.push('\n');
-                                        } else {
-                                            let mention = format! {"~~{}~~\n", player.0.mention()};
-                                            players.push_str(&mention);
-                                        }
-                                    }
-                                    players
-                                }
-                            })(),
-                            true,
-                        )
+                        .fields(fields)
                         .colour((|| {
                             if game.state() == GameState::NotStarted {
                                 if game.can_start() {
@@ -88,8 +95,6 @@ pub async fn game_info(ctx: &Context, msg: &Message) -> CommandResult {
                                 0x0dd910 // Green
                             }
                         })())
-                        .field("Meeting room", game.meeting_room().mention(), true)
-                        .field("Player role", game.player_role().mention(), true)
                         .footer(|f| {
                             if let Some(ava) = msg.author.avatar_url() {
                                 f.icon_url(ava);
