c13087826a44b4c795bd0ba15af61bc548df967d
RealKC <dumitru0mitca@gmail.com>
2020-11-03 13:55:11 +0200

    Do not allow players to leave a running game
    Fixes #17

--- src/commands/game/leave.rs
+++ src/commands/game/leave.rs
@@ -1,3 +1,5 @@
+use crate::game::GameState;
+
 use super::prelude::*;
 
 use tracing::info;
@@ -12,14 +14,21 @@ pub async fn leave(ctx: &Context, msg: &Message) -> CommandResult {
 
     if game.is_some() {
         let mut game = game.unwrap().write().await;
-        let result = game.leave(msg.author.id);
-        if result.is_ok() {
-            info!("A user succesfully left a game");
-            msg.reply_err(ctx, "you've successfully left the game :c".into())
+        if game.state() != GameState::NotStarted || game.state() != GameState::GameEnded {
+            msg.reply_err(ctx, "you can't leave a game that has started!".into())
                 .await?;
+            info!("User tried leaving running game");
+            return Ok(());
         } else {
-            info!("User couldn't leave, error is {:?}", result.unwrap_err());
-            msg.reply(ctx, format!("{}", result.unwrap_err())).await?;
+            let result = game.leave(msg.author.id);
+            if result.is_ok() {
+                info!("A user succesfully left a game");
+                msg.reply_err(ctx, "you've successfully left the game :c".into())
+                    .await?;
+            } else {
+                info!("User couldn't leave, error is {:?}", result.unwrap_err());
+                msg.reply(ctx, format!("{}", result.unwrap_err())).await?;
+            }
         }
     } else {
         msg.reply_err(ctx, "you can't leave a game if you aren't in one!".into())
