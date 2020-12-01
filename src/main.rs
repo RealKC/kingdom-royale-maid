a2e0341879f8b7c33f82db7be4e31da2365b3ad7
Mițca Dumitru <dumitru0mitca@gmail.com>
2020-12-01 13:17:24 +0200

    Remove Windows support
    🦀 Windex is gone 🦀
    
    ... memes aside, this should make developing some specific features
    easier, since I won't have to care about them working on Windows too

--- src/main.rs
+++ src/main.rs
@@ -120,37 +120,21 @@ async fn main() -> CommandResult {
 
     // Listen to interrupts
     // Thanks Prof Bloodstone from the serenity discord
-    #[cfg(not(windows))]
-    {
-        use tokio::{signal::unix::signal, signal::unix::SignalKind};
 
-        let signals_to_handle = vec![
-            SignalKind::hangup(),
-            SignalKind::interrupt(),
-            SignalKind::terminate(),
-        ];
-        for kind in signals_to_handle {
-            let mut stream = signal(kind).unwrap();
-            let shard_manager = client.shard_manager.clone();
-            tokio::spawn(async move {
-                stream.recv().await;
-                info!("Signal received - shutting down!");
-                shard_manager.lock().await.shutdown_all().await;
-            });
-        }
-    }
+    use tokio::{signal::unix::signal, signal::unix::SignalKind};
 
-    #[cfg(windows)]
-    {
-        use tokio::signal::windows::ctrl_break;
-
-        let mut stream = ctrl_break();
+    let signals_to_handle = vec![
+        SignalKind::hangup(),
+        SignalKind::interrupt(),
+        SignalKind::terminate(),
+    ];
+    for kind in signals_to_handle {
+        let mut stream = signal(kind).unwrap();
         let shard_manager = client.shard_manager.clone();
-
         tokio::spawn(async move {
-            stream.unwrap().recv().await;
-            info!("Ctrl Break received - shutting down!");
-            shard_manager.lock().await.shutdown_all.await;
+            stream.recv().await;
+            info!("Signal received - shutting down!");
+            shard_manager.lock().await.shutdown_all().await;
         });
     }
 
